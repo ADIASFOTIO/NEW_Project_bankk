afbcdef6de63edc9e91c25e8872dc221a7e5be81
adias <jnrfotio@gmail.com>
2024-06-16 12:52:12 +0200

    init commit

---
+++ projectbankk/projectbankk/src/main/java/it/bankproject/projectbankk/ProjectbankkApplication.java
@@ -0,0 +1,13 @@
+package it.bankproject.projectbankk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectbankkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectbankkApplication.class, args);
+	}
+
+}
